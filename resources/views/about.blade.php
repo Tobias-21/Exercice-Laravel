e76ffe201ac9a8dc0cb27af0401e31202f33ef76
Tobias-21 <onellemonsiotojisca@gmail.com>
2025-07-30 13:09:22 +0100

    Étape 1 : intégration des pages HTML en vues Blade avec routes simples

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>About</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <nav>
+      <a href="/">Home</a>
+      <a href="about">About</a>
+      <a href="contact">Contact</a>
+    </nav>
+  </header>
+  <main>
+    <h1>Page About</h1>
+  </main>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Home</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <nav>
+      <a href="/">Home</a>
+      <a href="about">About</a>
+      <a href="contact">Contact</a>
+    </nav>
+  </header>
+  <main>
+    <h1>Page Home</h1>
+  </main>
+</body>
+</html>
