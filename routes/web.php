8ad82398442418380e39f92d59a140cea2832ede
Tobias-21 <onellemonsiotojisca@gmail.com>
2025-07-30 13:34:31 +0100

    Étape 2 : mise en place du contrôleur PageController pour gérer les vues

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [PageController::class, 'home']);
 
-Route::get('/about', function () {
-    return view('about');
-});
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/about', [PageController::class, 'about']);
+
+Route::get('/contact', [PageController::class, 'contact']);
 
